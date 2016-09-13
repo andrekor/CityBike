afe21942c829f913e2c5070ecd999f1423986eef
André Kramer Orten <andreko@broadpark.no>
2016-09-13 23:30:54 +0200

    added date conversion from weather data

--- bysykkelScript.R
+++ bysykkelScript.R
@@ -15,10 +15,15 @@ availabilityData <- readBysykkelFiles("Downloads/data/availability/")
 #Make weather data ready
 weather  <- readWeatherFiles("Downloads/data/weather/")
 weather$temperature <- as.double(levels(weather$temperature))[weather$temperature] #Converting from factor to double
+weather$timeStamp <- as.POSIXct(weather$timeStamp, format="%Y-%m-%dT%H:%M:%SZ")
 
 #Merge the availability data with the station data for more description and info 
 descriptionData <- merge(availabilityData, station_data$stations[c("id", "title")], by="id")
 
+#Need to make a function to fill in the temperature to nearest hour for the descriptionData
+
+
+#Test*************************************************
 #Majorstua has id: 189. 
 majorstua <- availabilityData[availabilityData$id==189,]
 
@@ -30,3 +35,5 @@ ggplot(data=availabilityData[availabilityData$id==189,], aes_string(x="time", y=
 
 ggplot(data=weather[15:32,], aes(x=timeStamp, y=temperature, fill=temperature)) + 
   geom_point(shape=1) + theme(axis.text.x = element_text(angle = 30, hjust = 1))
+
+
